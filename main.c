67b807949e5f27a02459829367555e27d9e08185
RoniDomi <roni.domi001@umb.edu>
2024-05-02 20:29:27 -0400

    Arithmetic operations functional*

--- main.c
+++ main.c
@@ -3,7 +3,47 @@
 
 // Function that handles inputs for simple operations involving + - * /
 void arithmeticOperations() {
-    printf("here");
+    printf("\n\n\n--------------- Arithmetic ---------------\n");
+    printf("Enter one number and operator at a time!\n*Press '=' to see results. \n");
+
+    char operation[] = "";
+    double initialNum = 0;
+
+    printf("Nr: ");
+    scanf("%lf", &initialNum); // Get first number
+
+    while(1) {
+        printf("Operator: ");
+        scanf("%s", &operation); // Get arithmetic operator
+
+        // Check if user requested termination of operations
+        if (strcmp(operation, "=") == 0) {
+            printf("Total = %.5lf\n", initialNum);
+            return;
+        }
+
+        // Get next number
+        double currentNum = 0;
+        printf("Next nr: ");
+        scanf("%lf", &currentNum);
+
+        // Based on entered operation update value
+        if (strcmp(operation, "+") == 0) {
+            initialNum += currentNum;
+        }
+        else if (strcmp(operation, "-") == 0) {
+            initialNum -= currentNum;
+        }
+        else if (strcmp(operation, "*") == 0) {
+            initialNum *= currentNum;
+        }
+        else if (strcmp(operation, "/") == 0) {
+            initialNum /= currentNum;
+        }
+        else {
+            printf("Invalid operator entered!");
+        }
+    }
 }
 
 // Function that handles UI printing for calculator menu
@@ -15,6 +55,7 @@ void menu() {
     printf("To exit enter '.'\n");
     scanf("%s", &choice);
 
+    // Call respective function to take user to respective menu based on choice
     if (strcmp(choice, "A") == 0 || strcmp(choice, "a") == 0) {
         arithmeticOperations();
     }
@@ -22,6 +63,7 @@ void menu() {
         printf("E");
     }
     else if (strcmp(choice, ".") == 0) {
+        printf("Exiting...");
         return;
     }
     else {
