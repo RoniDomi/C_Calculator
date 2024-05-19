e7b4d3ac296e985a9d4bede8382e4cceff998c90
RoniDomi <roni.domi001@umb.edu>
2024-05-19 01:33:16 -0400

    Finished grid and digit display.

--- appStructure.h
+++ appStructure.h
@@ -2,7 +2,7 @@
 #define C_CALCULATOR_APPSTRUCTURE_H
 #include <gtk/gtk.h>
 
-void print_console(GtkWidget *widget, gpointer data);
+void displayDigits(GtkWidget *widget, gpointer data);
 void activate(GtkApplication *app, gpointer user_data);
 
 #endif //C_CALCULATOR_APPSTRUCTURE_H
